95a93706e74291e67938f73df80baf19ebde533d
PangLi Yang <reli810436@gmail.com>
2024-12-23 01:50:42 -0500

    modify perm

--- update_permissions.sh
+++ update_permissions.sh
@@ -4,17 +4,17 @@
 BASE_DIR="/web/py2236"
 
 # Set permissions for the base directory and its contents
-chmod 711 "$BASE_DIR/weblab/"
+chmod 711 "$BASE_DIR/weblab_workshop/"
 
 # Set permissions for the files and directories in 'graphics'
-chmod 644 "$BASE_DIR/weblab/index.html"
+chmod 644 "$BASE_DIR/weblab_workshop/index.html"
 
 # Set permissions for homework directories and their contents
-chmod 711 "$BASE_DIR/weblab/ws0/"
-chmod 711 "$BASE_DIR/weblab/ws1/"
+chmod 711 "$BASE_DIR/weblab_workshop/ws0/"
+chmod 711 "$BASE_DIR/weblab_workshop/ws1/"
 
-chmod 644 "$BASE_DIR/weblab/ws0/"*
-chmod 644 "$BASE_DIR/weblab/ws1/"*
+chmod 644 "$BASE_DIR/weblab_workshop/ws0/"*
+chmod 644 "$BASE_DIR/weblab_workshop/ws1/"*
 
 
 echo "Permissions have been updated successfully."
